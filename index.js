d893b2c2278f92960948df31b4386b7d89ef63c4
Darren Kandekore <darren@kandekore.net>
2022-08-26 16:53:07 +0100

    class code copy

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-const fs = require('fs')
-const inquirer = require('inquirer')
+const fs = require("fs");
+const inquirer = require("inquirer")
 
-inquirer.prompt([{
-    name: "username",
-    message: "what is your name?",
-    type: "input",
-},])
+const generateHTML = ({name, github, linkedin}) => '<!DOCTYPE html><html lang="en"><head> <meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Document</title></head><body><h1>${name}</h1><p>${github}</p><p>${linkedin}</p></body></html>';
+
+inquirer
+  .prompt([
+    {
+      name: "username",
+      message: "what is your name?",
+      type: "input",
+    },
+    {
+      name: "github",
+      message: "what is your Git Hub URL??",
+      type: "input",
+    },
+    {
+      name: "linkedin",
+      message: "what is your Linkedin URL??",
+      type: "input",
+    }
+  ])
+  .then((response) => {
+    const text = generateHTML(response)
+    fs.writeFile("index.html". text, (err) => {err ? console.log(err) : console.log("success")}
+    )
+  });
